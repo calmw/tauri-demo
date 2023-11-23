624c083ca7a2783a062c03fe4a1322f6f9b184bc
calmw <calm.wang@hotmail.com>
2023-11-23 18:46:29 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// When using the Tauri API npm package:
-// import { invoke } from '@tauri-apps/api/tauri'
-// When using the Tauri global script (if not using the npm package)
-// Be sure to set `build.withGlobalTauri` in `tauri.conf.json` to true
 const invoke = window.__TAURI__.invoke
 
 $(function () {
